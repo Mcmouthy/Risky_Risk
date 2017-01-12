d327468b82af039c5c204ab3e4f71aca19e4d36b
HAFFNER Yoann <yoann.haffner@edu.univ-fcomte.fr>
2017-01-12 17:19:27 +0100

    Global MVC update

--- src/View/Game_View.java
+++ src/View/Game_View.java
@@ -2,7 +2,9 @@ package View;
 
 import Model.Partie;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -14,6 +16,7 @@ public class Game_View {
     private Menu_View menu_view;
 
     public Button endTurn;
+    public Label notice;
 
 
     public Game_View(Partie model, Stage stage) {
@@ -25,6 +28,7 @@ public class Game_View {
 
     private void initAttributs() {
         endTurn = new Button("Terminer le tour");
+        notice = new Label("");
     }
 
     public void setGameView() {
@@ -32,9 +36,11 @@ public class Game_View {
         ((BorderPane) stage.getScene().getRoot()).getChildren().clear();
 
 
+        HBox panel = new HBox();
+        panel.getChildren().add(endTurn);
+        panel.getChildren().add(notice);
 
         ((BorderPane) stage.getScene().getRoot()).setCenter(endTurn);
-
 
         stage.getScene().getRoot().setVisible(true);
     }
