7c74d46c49695ebc6d8581ed3e0cfe122a056f5b
PlatypusPT <adrienfilippidu31@hotmail.fr>
2017-01-12 22:50:23 +0100

    Add files via upload

--- src/Controller/Control_Menu.java
+++ src/Controller/Control_Menu.java
@@ -3,32 +3,34 @@ package Controller;
 
 import Model.Joueur;
 import Model.Partie;
-import View.Game_View;
 import View.Menu_View;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 /**
  * Created by yhaffner on 21/11/16.
  */
-public class Control_Menu implements EventHandler<ActionEvent> {
+public class Control_Menu implements EventHandler<MouseEvent>
+{
     private Menu_View view;
 
-    public Control_Menu(Menu_View menu) {
+    public Control_Menu(Menu_View menu)
+    {
         this.view = menu;
         this.view.setController(this);
 
         // En attendant un menu fonctionnel, pour passer direct au jeu:
-         Partie p = new Partie();
-         p.ajouterJoueur(new Joueur("Pierre",0));
-         p.ajouterJoueur(new Joueur("Gérard",1));
-         new Control_Game(p,this);
+        // Partie p = new Partie();
+        // p.ajouterJoueur(new Joueur("Pierre",0));
+        // p.ajouterJoueur(new Joueur("Gérard",1));
+        // new Control_Game(p,this);
         // fin truc inutile
     }
 
     @Override
     // TODO ULTRA IMPORTANT
-    public void handle(ActionEvent event) {
+    public void handle(MouseEvent event)
+    {
         /*
         Réception des objets du genre:
 
@@ -36,7 +38,21 @@ public class Control_Menu implements EventHandler<ActionEvent> {
             traiter...
             ex: view.setOptionsView();
         }
+        */
 
+        if (event.getSource().equals(getView().startButton))
+        {
+            view.setMainMenuView();
+        }
+
+        if (event.getSource().equals(getView().nouvellePartie))
+        {
+            Partie p = new Partie();
+            p.ajouterJoueur(new Joueur("Pierre",0));
+            p.ajouterJoueur(new Joueur("Gérard",1));
+            new Control_Game(p,this);
+        }
+/*
         pour le début de partie, il faudra juste créer ça:
                 Partie p = new Partie();
                 ajouter à p les joueurs
@@ -45,7 +61,8 @@ public class Control_Menu implements EventHandler<ActionEvent> {
          */
     }
 
-    public Menu_View getView() {
+    public Menu_View getView()
+    {
         return view;
     }
 }
